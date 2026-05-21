7854773d8caa5e34a3292976f8d334e4261605b3
HDaniloH <hco.danilo@gmail.com>
2026-05-21 15:22:08 -0300

    banco de conversas

--- Domínios.py
+++ Domínios.py
@@ -1,10 +1,12 @@
 from datetime import datetime
+import sqlite3
 class Usuario:
-    def __init__(self, nome, nome_usuario, senha, status,  foto_perfil = None):
+    def __init__(self, nome, nome_usuario, senha, status, endereco, foto_perfil = None):
         self.nome = nome
         self.nome_usuario = nome_usuario
         self.senha = senha
         self.status = status
+        self.endereco = endereco
         self.foto_perfil = foto_perfil
 
     def trocar_nome_usuario(self, nome_novo):
@@ -22,9 +24,9 @@ class Notificacao:
         self.data_hora = datetime.now()
 
 class Mensagem:
-    def __init__(self, Usuario_origem, Usuario_destino, mensagem = None):
+    def __init__(self, Usuario_origem: Usuario, Usuario_destino: Usuario, mensagem = None):
         self.mensagem = mensagem
         self.Usuario_origem = Usuario_origem
         self.Usuario_destino = Usuario_destino
     def enviar_mensagem(self):
-        print(f"<{self.Usuario_origem}><{self.Usuario_destino}>{self.mensagem}")
+        print(f"<{self.Usuario_origem.nome_usuario}><{self.Usuario_destino.nome_usuario}>{self.mensagem}")
--- Entidades.py
+++ Entidades.py
@@ -1,11 +1,23 @@
 from Domínios import *
 from Domínios import Mensagem
 
-
 class Conversa:
-    def __init__(self, usuario_origem, usuario_destino):
+    def __init__(self, usuario_origem: Usuario, usuario_destino: Usuario):
         self.usuario_origem = usuario_origem
         self.usuario_destino = usuario_destino
+    def criarConversa(self):
+        banco_conversa = sqlite3.connect("banco_conversa.db")
+        cursor = banco_conversa.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS conversas(id INTEGER PRIMARY KEY AUTOINCREMENT, usuario1_nome TEXT, usuario2_nome TEXT, UNIQUE(usuario1_nome, usuario2_nome))")
+        cursor.execute("CREATE TABLE IF NOT EXISTS mensagens(id INTEGER PRIMARY KEY AUTOINCREMENT, mensagem TEXT, conversa_id INTEGER, FOREIGN KEY (conversa_id) REFERENCES conversas(id))")
+        cursor.execute("INSERT OR IGNORE INTO conversas(usuario1_nome, usuario2_nome) VALUES('"+self.usuario_origem.nome_usuario+"', '"+self.usuario_destino.nome_usuario+"')")
+        banco_conversa.commit()
+        banco_conversa.close()
     def enviarMensagem(self, mensagem_nova):
-        mensagem = Mensagem(self.usuario_origem, self.usuario_destino, mensagem_nova)
-        mensagem.enviar_mensagem()
+        banco_conversa = sqlite3.connect("banco_conversa.db")
+        cursor = banco_conversa.cursor()
+        cursor.execute("SELECT * FROM conversas WHERE usuario1_nome = ? AND usuario2_nome = ?", (self.usuario_origem.nome_usuario, self.usuario_destino.nome_usuario,))
+        conversa = cursor.fetchone()
+        cursor.execute("INSERT INTO mensagens(mensagem, conversa_id) VALUES('"+mensagem_nova+"','"+str(conversa[0])+"')")
+        banco_conversa.commit()
+        banco_conversa.close()
