39811f050618b2c990a4d5add4094daa12d0d19e
YuriNeves148 <gut.hub.yuri.148@gmail.com>
2026-05-25 14:06:20 -0300

    configurando pages 2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
 import Home from "../src/paginas/home";
@@ -10,13 +10,13 @@ import Usuario from "./paginas/usuario";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/feed" element={<Feed />} />
         <Route path="/usuario" element={<Usuario />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
